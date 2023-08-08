97239b22c712fd28c91496d92f42e65f7fa7cf06
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-08-07 20:39:20 -0700

    Disabled image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
+  images: {
+    unoptimized: true
+  }
 })
 
 module.exports = withNextra()
