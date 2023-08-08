e2eeb06f6d0fe5dd3dff3987e1deafdb866d5c37
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-08-07 22:55:55 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.js',
 })
 module.exports = withNextra({
-  basePath: '/mineflayer-guide',
+  basePath: process.env.NODE_ENV === 'development' ? '' : '/mineflayer-guide',
   images: {
     unoptimized: true
   }
