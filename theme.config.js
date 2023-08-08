25751847e8de7edb7919a80702878353b869601e
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-08-07 22:06:25 -0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -46,7 +46,7 @@ const config = {
   toc: {
     float: true
   },
-  docsRepositoryBase: 'https://github.com/Pix3lPirat3/mineflayer-guide/',
+  docsRepositoryBase: 'https://github.com/Pix3lPirat3/mineflayer-guide/tree/main/',
   feedback: {
     content: null
   },
